5cd1c0f5b5338317a8476849d8c1e31746018c7f
MD. Amzad Hossain Arif <arifamzad@Arifs-MacBook-Pro.local>
2018-12-08 12:42:03 +0600

    Initial commit

---
+++ app/src/main/java/com/arifamzad/you/MainActivity.java
@@ -0,0 +1,30 @@
+package com.arifamzad.you;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.b1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://youtube.com"));
+                startActivity(i);
+            }
+        });
+    }
+}
